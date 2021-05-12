5844a05709bf421cc331451f7da35547e8243bf7
codesyariah122 <pujiermanto@gmail.com>
2021-05-12 22:07:52 +0700

    update hero & parallax all app

--- components/Aboutpage/Hero.vue
+++ components/Aboutpage/Hero.vue
@@ -59,7 +59,7 @@
 .jumbotron-content .container {
   z-index: 1;
   position: relative;
-  margin-left: 5rem!important;
+  margin-left: 1rem!important;
 }
 
 .jumbotron-content::after {
@@ -90,7 +90,7 @@
 
 .jumbotron-content p {
   font-weight: 500;
-  font-size: 35px;
+  font-size: 23px;
   color: #FFFFF0;
   font-family: "Poiret One", cursive;
   font-weight: bold;
