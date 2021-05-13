709c57fb76843ac80e5c6d5220197cac21875cf0
codesyariah122 <pujiermanto@gmail.com>
2021-05-13 14:17:17 +0700

    reduce all image hero & parallax

--- components/Aboutpage/Hero.vue
+++ components/Aboutpage/Hero.vue
@@ -43,17 +43,16 @@
 .jumbotron-content {
   /* background-size: cover;
   height: 540px; */
-  /*text-align: center;*/
+  text-align: center;
   position: relative;
   background-color: #17234E;
   min-height: 100vh;
   width: 100%;
-  background-repeat: no-repeat;
-  background-position: center;
   -webkit-background-size: cover;
   background-size: cover;
   position: relative;
   clip-path: polygon(50% 0%, 100% 0, 100% 89%, 92% 92%, 84% 94%, 50% 100%, 0 100%, 0 59%, 0 0);
+  z-index: -1!important;
 }
 
 .jumbotron-content .container {
@@ -73,11 +72,11 @@
 }
 
 .jumbotron-content .display-4 {
-  width: 100%;
   color: white;
-  /*text-align: center;*/
+  text-align: center;
+  margin-top: 9rem;
   font-weight: 200;
-  text-shadow: 1px 1px 1px rgba(0, 0, 0, 1.9);
+  text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.5);
   margin-bottom: 30px;
   font-family: "Poppins", sans-serif;
 }
@@ -89,26 +88,25 @@
 
 .jumbotron-content p {
   font-weight: 500;
-  font-size: 16px;
+  font-size: 21px;
   color: #FFFFF0;
   font-family: "Poiret One", cursive;
   font-weight: bold;
-  margin-left: .7rem!important;
 }
 
 /* DESKTOP VERSION */
 @media (min-width: 992px) { 
-	/* jumbotron */
-	.jumbotron-content{
-		margin-top: -75px;
-		height: 840px;
-	}
-  .jumbotron-content .container{
-    margin-top: 13rem!important;
+  /* jumbotron */
+  .jumbotron-content{
+    margin-top: -75px;
+    height: 840px;
+    z-index: -1!important;
   }
-	.jumbotron-content .display-4{
-		text-transform: capitalize;
-     text-shadow: 0 3px 20px rgba(0, 0, 0, 1.9)
-	}
+  .jumbotron-content .display-4{
+    text-transform: capitalize;
+  }
+  .jumbotron-content p{
+    font-size: 31px!important;
+  }
 }
 </style>
