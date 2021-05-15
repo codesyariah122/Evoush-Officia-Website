086cc1945bdfc940ddd480811c7b4192bed860f6
codesyariah122 <pujiermanto@gmail.com>
2021-05-15 22:43:33 +0700

    update product

--- components/ProductList.vue
+++ components/ProductList.vue
@@ -3,12 +3,26 @@
     <div id="product-list">
       <div class="container mt-5">
         <div class="row justify-content-start">
+          <div class="col-12">
+            <h1>
+              <n-link to="/categories/nutrisi">
+                Nutrisi
+              </n-link>
+            </h1>
+          </div>
           <div v-for="product in products" :key="product.permalink" v-if="product.categories[0].name == 'Nutrisi'" class="col-md-4">
              <NutrisiView :product="product" :categories="product.categories[0]"/>
           </div>
           </div>
           <hr>
           <div class="row justify-content-start">
+            <div class="col-12">
+               <h1>
+                <n-link to="/categories/cosmetics">
+                  Cosmetics
+                </n-link>
+              </h1>
+            </div>
              <div v-for="product in products" :key="product.permalink" v-if="product.categories[0].name == 'Cosmetics'" class="col-md-4">
              <CosmeticsView :product="product" :categories="product.categories[0]"/>
           </div>
