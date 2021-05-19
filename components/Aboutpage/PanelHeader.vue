6ae56165de6d9a4623c13f85e41ec670ed72c219
codesyariah122 <pujiermanto@gmail.com>
2021-05-20 00:19:16 +0700

    update all page

--- components/Aboutpage/PanelHeader.vue
+++ components/Aboutpage/PanelHeader.vue
@@ -153,8 +153,8 @@
       width: 100%;
     }
     .panel-body-header img{
-      width: 400px;
-      height: 500px;
+      width: 400px!important;
+      /*height: 200px!important;*/
       box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)!important; color: rgb(255,228,181);
     }
     .panel-body-header h1{
