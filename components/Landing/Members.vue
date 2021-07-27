739b12fec18d28543bafbc204aed10757b351248
codesyariah122 <pujiermanto@gmail.com>
2021-07-27 23:44:04 +0700

    update component member

--- components/Landing/Members.vue
+++ components/Landing/Members.vue
@@ -20,7 +20,8 @@
 									<path stroke="none" stroke-width="0" fill="#f5f5f5" d="M300,521.0016835830174C376.1290562159157,517.8887921683347,466.0731472004068,529.7835943286574,510.70327084640275,468.03025145048787C554.3714126377745,407.6079735673963,508.03601936045806,328.9844924480964,491.2728898941984,256.3432110539036C474.5976632858925,184.082847569629,479.9380746630129,96.60480741107993,416.23090153303,58.64404602377083C348.86323505073057,18.502131276798302,261.93793281208167,40.57373210992963,193.5410806939664,78.93577620505333C130.42746243093433,114.334589627462,98.30271207620316,179.96522072025542,76.75703585869454,249.04625023123273C51.97151888228291,328.5150500222984,13.704378332031375,421.85034740162234,66.52175969318436,486.19268352777647C119.04800174914682,550.1803526380478,217.28368757567262,524.383925680826,300,521.0016835830174"></path>
 								</svg>
 								<!-- <i class="bx bxl-dribbble"></i> -->
-								<img :src="`https://app.evoush.com/storage/${member.avatar}`" class="img-fluid img-responsive" width="150">
+								<img v-if="member.avatar" :src="`https://app.evoush.com/storage/${member.avatar}`" class="img-fluid img-responsive" width="150">
+								<img v-else src="https://raw.githubusercontent.com/codesyariah122/bahan-evoush/main/images/profile/default.jpg" :alt="member.name" class="image--profile-member img-responsive rounded-circle center-block d-block mx-auto mt-2" width="100">
 							</div>
 							<div class="mt-5">
 								<h4><a href="">{{member.name}}</a></h4>
@@ -38,7 +39,27 @@
 										</div>
 									</li>
 								</ul>
-								<p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+								<div v-if="member.quotes">
+									<blockquote class="blockquote-footer">
+										{{member.quotes}}
+										<br>
+										<small style='text-transform: capitalize;' class="text-secondary">
+											Quotes by : {{member.name}}
+										</small>
+									</blockquote>
+								</div>
+								<div v-else>
+									<blockquote class="blockquote-footer">
+										Belum Menambahkan Quotes, silahkan edit profile <br>
+										<small style='text-transform: capitalize;' class="text-secondary">
+											{{member.name}} Belum menambahkan quotes
+										</small>
+									</blockquote>
+								</div>
+
+								<div class="mt-5">
+									<nuxt-link :to="{name:'member-username', params: {username: member.username}}" class="btn btn-primary"><i class='bx bx-link-external text-white'></i> Lihat Profile</nuxt-link>
+								</div>
 							</div>
 						</div>
 					</div>
